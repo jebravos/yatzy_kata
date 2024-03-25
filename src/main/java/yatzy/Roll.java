bd113871fc9eff745662a3320d7d309e3380e371
Jorge Esteban BRAVO SANCHEZ <jebravo@ippon.fr>
2024-03-25 16:03:47 +0100

    refactor: move Ãmethods getCounts, sumDiceValues and sumDiceValuesWhen to Roll class

--- src/main/java/yatzy/Roll.java
+++ src/main/java/yatzy/Roll.java
@@ -1,4 +1,26 @@
 package yatzy;
 
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
+
 public record Roll(int d1, int d2, int d3, int d4, int d5) {
+    public int[] getCounts() {
+        int[] counts = new int[6];
+        counts[d1 - 1]++;
+        counts[d2 - 1]++;
+        counts[d3 - 1]++;
+        counts[d4 - 1]++;
+        counts[d5 - 1]++;
+        return counts;
+    }
+
+    public int sumDiceValues() {
+        return sumDiceValuesWhen(die -> true);
+    }
+
+    public int sumDiceValuesWhen(IntPredicate condition) {
+        return IntStream.of(d1, d2, d3, d4, d5)
+                .filter(condition)
+                .sum();
+    }
 }
