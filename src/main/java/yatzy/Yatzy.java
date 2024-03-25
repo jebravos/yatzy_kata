6a3d16531965ea2327d9a0a0e083f1a7118dc011
Jorge Esteban BRAVO SANCHEZ <jebravo@ippon.fr>
2024-03-25 14:40:46 +0100

    refactor: rename variables to use same word for counts

--- src/main/java/yatzy/Yatzy.java
+++ src/main/java/yatzy/Yatzy.java
@@ -128,6 +128,7 @@ public class Yatzy {
         counts[d3 - 1]++;
         counts[d4 - 1]++;
         counts[d5 - 1]++;
+
         int n = 0;
         int score = 0;
         for (int i = 0; i < 6; i += 1)
@@ -140,52 +141,56 @@ public class Yatzy {
     }
 
     public static int threeOfAKind(int d1, int d2, int d3, int d4, int d5) {
-        int[] t;
-        t = new int[6];
-        t[d1 - 1]++;
-        t[d2 - 1]++;
-        t[d3 - 1]++;
-        t[d4 - 1]++;
-        t[d5 - 1]++;
+        int[] counts;
+        counts = new int[6];
+        counts[d1 - 1]++;
+        counts[d2 - 1]++;
+        counts[d3 - 1]++;
+        counts[d4 - 1]++;
+        counts[d5 - 1]++;
+
         for (int i = 0; i < 6; i++)
-            if (t[i] >= 3) return (i + 1) * 3;
+            if (counts[i] >= 3) return (i + 1) * 3;
         return 0;
     }
 
     public static int fourOfAKind(int d1, int d2, int d3, int d4, int d5) {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1 - 1]++;
-        tallies[d2 - 1]++;
-        tallies[d3 - 1]++;
-        tallies[d4 - 1]++;
-        tallies[d5 - 1]++;
+        int[] counts;
+        counts = new int[6];
+        counts[d1 - 1]++;
+        counts[d2 - 1]++;
+        counts[d3 - 1]++;
+        counts[d4 - 1]++;
+        counts[d5 - 1]++;
+
         for (int i = 0; i < 6; i++)
-            if (tallies[i] >= 4) return (i + 1) * 4;
+            if (counts[i] >= 4) return (i + 1) * 4;
         return 0;
     }
 
     public static int smallStraight(int d1, int d2, int d3, int d4, int d5) {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1 - 1] += 1;
-        tallies[d2 - 1] += 1;
-        tallies[d3 - 1] += 1;
-        tallies[d4 - 1] += 1;
-        tallies[d5 - 1] += 1;
-        if (tallies[0] == 1 && tallies[1] == 1 && tallies[2] == 1 && tallies[3] == 1 && tallies[4] == 1) return 15;
+        int[] counts;
+        counts = new int[6];
+        counts[d1 - 1] += 1;
+        counts[d2 - 1] += 1;
+        counts[d3 - 1] += 1;
+        counts[d4 - 1] += 1;
+        counts[d5 - 1] += 1;
+
+        if (counts[0] == 1 && counts[1] == 1 && counts[2] == 1 && counts[3] == 1 && counts[4] == 1) return 15;
         return 0;
     }
 
     public static int largeStraight(int d1, int d2, int d3, int d4, int d5) {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1 - 1] += 1;
-        tallies[d2 - 1] += 1;
-        tallies[d3 - 1] += 1;
-        tallies[d4 - 1] += 1;
-        tallies[d5 - 1] += 1;
-        if (tallies[1] == 1 && tallies[2] == 1 && tallies[3] == 1 && tallies[4] == 1 && tallies[5] == 1) return 20;
+        int[] counts;
+        counts = new int[6];
+        counts[d1 - 1] += 1;
+        counts[d2 - 1] += 1;
+        counts[d3 - 1] += 1;
+        counts[d4 - 1] += 1;
+        counts[d5 - 1] += 1;
+
+        if (counts[1] == 1 && counts[2] == 1 && counts[3] == 1 && counts[4] == 1 && counts[5] == 1) return 20;
         return 0;
     }
 
